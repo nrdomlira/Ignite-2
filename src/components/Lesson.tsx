b6666c71c1835e9cedc2fa2d05c28fbc74782e30
José Domingos <domliraa@gmail.com>
2022-06-22 12:51:30 -0300

    component aside completed

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,12 +1,24 @@
 import { CheckCircle, Lock } from "phosphor-react";
+import {format, isPast} from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR'
 
-export function Lesson() {
-    const isLessonAvaliable = false;
+interface LessonProps {
+    title: string;
+    slug: string;
+    availableAt: Date;
+    type: 'live' | 'class';
+}
+
+export function Lesson(props: LessonProps) {
+    const isLessonAvaliable = isPast(props.availableAt);
+    const lessonAvailableDateFormatted = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm",{
+        locale: ptBR
+    })
 
     return (
         <a href="#">
             <span className="text-gray-300">
-                Segunda • 21 de junho • 19h00
+                {lessonAvailableDateFormatted}
             </span>
 
             <div className="rounded border border-gray-500 p-4 mt-2">
@@ -24,12 +36,12 @@ export function Lesson() {
                     )}
 
                     <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold">
-                        Ao Vivo
+                        {props.type === 'live' ? 'Ao Vivo' : 'Aula Prática'}
                     </span>
                 </header>
 
                 <strong className="text-gray-200 mt-5 block">
-                    Abertura do evento Ignite Lab
+                    {props.title}
                 </strong>
             </div>
         </a>
